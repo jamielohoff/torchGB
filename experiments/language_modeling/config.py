37d13b94a30559ff8b639bfa9f7e4f973718dab7
Jamie Lohoff <j.grieser@fz-juelich.de>
2024-07-14 00:00:15 +0000

    e with 'origin/experiments'.

--- experiments/language_modeling/config.py
+++ experiments/language_modeling/config.py
@@ -16,27 +16,39 @@ def commit_to_experiments_branch(project_root: str):
     print("Committing current codebase to the `experiments` branch...")
     # Open the repository
     repo = git.Repo(project_root)
+    
+    # Stash changes
+    repo.git.stash("save")
+    
+    # Get the experiments branch
+    experiments_branch = repo.branches["experiments"]
 
-    # Check if there are any untracked files
-    if repo.is_dirty(untracked_files=True):
-        # Get the experiments branch
-        experiments_branch = repo.branches["experiments"]
+    # Checkout the experiments branch
+    repo.git.checkout("experiments")
+    
+    # Pop the stash
+    repo.git.stash("pop")
 
-        # Checkout the experiments branch
-        repo.git.checkout("experiments")
-        
+    if repo.is_dirty(untracked_files=True): 
         # Add all changes to the staging area
         repo.git.add(all=True)
 
         # Commit the changes to the experiments branch
         repo.git.commit(message="Auto-commit to experiments branch.")
-        
+
         # Push the changes to the remote repository
         repo.remote().push(experiments_branch)
 
-        # Get the commit hash values
+    # Get the commit hash values
     commit_hash = repo.head.commit.hexsha
     print(f"Commit hash: {commit_hash}")
+    
+    # Go back to main branch
+    experiments_branch = repo.branches["main"]
+
+    # Checkout the experiments branch
+    repo.git.checkout("main")
+    
     return commit_hash
             
     
