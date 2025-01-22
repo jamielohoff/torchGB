6e2b0190010480e13b609cc1f069e37a4d07c5e2
Jamie Lohoff <j.grieser@fz-juelich.de>
2025-01-22 23:16:18 +0100

    Automatic API summaries

--- docs/conf.py
+++ docs/conf.py
@@ -231,6 +231,9 @@ html_static_path = ["_static"]
 # Output file base name for HTML help builder.
 htmlhelp_basename = "torchGB-doc"
 
+# Automatically generate API summaries
+autosummary_generate = True
+
 
 # -- Options for LaTeX output ------------------------------------------------
 
